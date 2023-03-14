52d37bfb395555b2477b6d6095a359e001fa0bf1
Guilherme Bandeira <guilhermeprestes1@hotmail.com>
2023-03-14 15:26:31 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace InstallerMTW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to MTW Installer!\n "+
+                "Do you wish to install the following packages?\n" +
+                "");
+            Console.ReadKey();
+        }
+    }
+}
