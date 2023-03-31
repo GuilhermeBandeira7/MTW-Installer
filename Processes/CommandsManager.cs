c0428b4bac6ba2ab919b793abbae2614b0a65565
Guilherme Bandeira <guilhermeprestes1@hotmail.com>
2023-03-31 09:48:11 -0300

    Functionality of starting a process to execute bash commands implemented.

---
+++ Processes/CommandsManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+
+namespace InstallerMTW.Processes
+{
+    /// <summary>
+    /// Manager of all commands required to be executed by the Program.
+    /// </summary>
+    public class CommandsManager
+    {
+        /// <summary>
+        /// initialize a process on linux os.
+        /// </summary>
+        private Process systemProcess { get; set; }
+
+        public Process SystemProcess
+        {
+            get
+            {
+                if (systemProcess == null)
+                    return systemProcess;
+                else { throw new ProcessException("This process was already initialized."); }
+            }
+        }
+
+        public CommandsManager()
+        {
+            systemProcess = new Process();
+        }
+
+        /// <summary>
+        /// Opens an OS terminal and executes a specified command.
+        /// </summary>
+        /// <param name="filePath">Path to the executable file of the shell.</param>
+        /// <param name="command">Specified command to execute on the terminal.</param>
+        public void OpenApplication(string filePath, string command)
+        {
+            using(systemProcess)
+            {
+                systemProcess.StartInfo.FileName = filePath;
+                systemProcess.StartInfo.Arguments = command;
+                systemProcess.StartInfo.UseShellExecute = true;
+                systemProcess.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+                systemProcess.Start();
+            }            
+        }
+
+    }
+}
