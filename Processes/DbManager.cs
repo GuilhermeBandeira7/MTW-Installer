b99d6f299af06b1d356f843d3fe40b52d33100f6
Guilherme Bandeira <guilhermeprestes1@hotmail.com>
2023-06-09 15:59:59 -0300

    Rearrange of the record method with new features implemented.

---
+++ Processes/DbManager.cs
@@ -0,0 +1,20 @@
+﻿using EntityMtwServer;
+using EntityMtwServer.Entities;
+
+namespace InstallerMTW.Processes {
+    public static class DbManager {
+
+        private static MasterServerContext _context;
+
+        public static IEnumerable<string> GetPrimaryRtsp() {
+            HashSet<string> result = new HashSet<string>();
+            foreach (var rtsp in _context.Equipments) {
+                if (rtsp != null) {
+                    result.Add(rtsp.PrimaryRtsp);
+                }
+                return result;
+            }
+            throw new ProcessException("Coundn't find any rtsp on the database.");
+        }
+    }
+}
